fbf558e4c4967930f1c14c3df7c90545421def40
sadhjkawh <sadhjkawh@example.com>
2026-03-11 11:34:58 +0800

    chore: ignore generated app artifacts

--- mail-server/apps/webmail/.next/static/chunks/app/mail/settings/preferences/page-0d5e8ff196c1754f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[122],{3612:(e,s,_)=>{Promise.resolve().then(_.bind(_,9308))}},e=>{e.O(0,[564,510,96,308,5,212,358],()=>e(e.s=3612)),_N_E=e.O()}]);
